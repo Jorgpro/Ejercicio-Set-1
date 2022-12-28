5ca0a2504ec667ed693749e186ad5dc604778940
Jorgpro <115546930+Jorgpro@users.noreply.github.com>
2022-12-27 22:10:41 -0300

    Create EjercicioSet.js

---
+++ EjercicioSet.js
@@ -0,0 +1,7 @@
+const listaFamilia=["Jorge","Marcela","Uriel","Ian","Teo","Mika","Marcelo","Dylan",];
+const miSet= new Set(listaFamilia);
+
+miSet.add("Jorge");
+console.log(miSet)
+miSet.add("Javascript");
+console.log(miSet)
